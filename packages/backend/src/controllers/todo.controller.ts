e62aef59468c1063114568e76f151da568370672
vmakar9 <reinzein549@gmail.com>
2024-05-22 17:24:42 +0300

    CRUD Backend and Middleware

--- packages/backend/src/controllers/todo.controller.ts
+++ packages/backend/src/controllers/todo.controller.ts
@@ -1,28 +1,21 @@
-import { Response, Request, NextFunction } from 'express';
+import { Response, Request } from 'express';
 import TodoService from '../services/todo.service';
 import { Todo } from '@/types/todos.type';
+import { ApiError } from '@/error/api.error';
 
 export class TodoController {
 	constructor(private todoService: TodoService) {}
 
-	async getAllTodo(
-		_: Request,
-		res: Response,
-		next: NextFunction,
-	): Promise<void> {
+	async getAllTodo(_: Request, res: Response): Promise<void> {
 		try {
 			const todos = await this.todoService.findAll();
 			res.send(todos);
 		} catch (e) {
-			next(e);
+			throw new ApiError('Something went wrong', 500);
 		}
 	}
 
-	async createTodo(
-		req: Request,
-		res: Response,
-		next: NextFunction,
-	): Promise<void> {
+	async createTodo(req: Request, res: Response): Promise<void> {
 		try {
 			const { title, completed, description } = req.body;
 			const todo = await this.todoService.createTodo(
@@ -32,50 +25,38 @@ export class TodoController {
 			);
 			res.json(todo);
 		} catch (e) {
-			next(e);
+			throw new ApiError('Something went wrong', 500);
 		}
 	}
 
-	async getById(
-		req: Request,
-		res: Response,
-		next: NextFunction,
-	): Promise<void> {
+	async getById(req: Request, res: Response): Promise<void> {
 		try {
 			const { id } = req.params;
 			const todo = await this.todoService.getTodoById(Number(id));
 			res.json(todo);
 		} catch (e) {
-			next(e);
+			throw new ApiError('Something went wrong', 500);
 		}
 	}
 
-	async updateTodo(
-		req: Request,
-		res: Response,
-		next: NextFunction,
-	): Promise<void> {
+	async updateTodo(req: Request, res: Response): Promise<void> {
 		try {
 			const { id } = req.params;
 			const data: Partial<Todo> = req.body;
 			const todo = await this.todoService.updateTodo(Number(id), data);
 			res.json(todo);
 		} catch (e) {
-			next(e);
+			throw new ApiError('Something went wrong', 500);
 		}
 	}
 
-	async deleteTodo(
-		req: Request,
-		res: Response,
-		next: NextFunction,
-	): Promise<void> {
+	async deleteTodo(req: Request, res: Response): Promise<void> {
 		try {
 			const { id } = req.params;
 			await this.todoService.deleteTodo(Number(id));
 			res.json('Todo is deleted');
 		} catch (e) {
-			next(e);
+			throw new ApiError('Something went wrong', 500);
 		}
 	}
 }
--- packages/backend/src/services/todo.service.ts
+++ packages/backend/src/services/todo.service.ts
@@ -1,14 +1,10 @@
 import { prisma } from '../prismaclient/prisma.client';
-import { ApiError } from '../error/api.error';
+
 import { Todo } from '../types/todos.type';
 
 export default class TodoService {
 	async findAll(): Promise<Todo[]> {
-		try {
-			return prisma.todo.findMany();
-		} catch (e) {
-			throw new ApiError('Todos not found ', 404);
-		}
+		return prisma.todo.findMany();
 	}
 
 	async createTodo(
@@ -16,43 +12,27 @@ export default class TodoService {
 		completed: boolean,
 		description: string,
 	): Promise<Todo> {
-		try {
-			return prisma.todo.create({
-				data: { title, completed, description },
-			});
-		} catch (e) {
-			throw new ApiError('Error create a todo', 422);
-		}
+		return prisma.todo.create({
+			data: { title, completed, description },
+		});
 	}
 
 	async getTodoById(id: number): Promise<Todo | null> {
-		try {
-			return prisma.todo.findUnique({
-				where: { id: Number(id) },
-			});
-		} catch (e) {
-			throw new ApiError('Error found a todo', 400);
-		}
+		return prisma.todo.findUnique({
+			where: { id: Number(id) },
+		});
 	}
 
 	async updateTodo(id: number, data: Partial<Todo>): Promise<Todo> {
-		try {
-			return prisma.todo.update({
-				where: { id },
-				data,
-			});
-		} catch (e) {
-			throw new ApiError('Error to update todo', 400);
-		}
+		return prisma.todo.update({
+			where: { id },
+			data,
+		});
 	}
 
 	async deleteTodo(id: number): Promise<void> {
-		try {
-			await prisma.todo.delete({
-				where: { id },
-			});
-		} catch (e) {
-			throw new ApiError('Error to delete todo', 400);
-		}
+		await prisma.todo.delete({
+			where: { id },
+		});
 	}
 }
